2f1407c54f9e5d8ecc742b1311af133efe5ceb8c
Isaías <23620031@tlaxiaco.tecnm.mx>
2025-03-19 12:10:18 -0600

    BIBLIOTECA AÑADIDA

---
+++ Biblioteca_INE/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_INE
+{
+    public class Class1
+    {
+        //BIBLIOTECA AÑADIDA
+    }
+}
