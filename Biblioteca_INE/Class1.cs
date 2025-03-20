6662f9e5768003c7111adc80e02e83a878863070
Isaías <23620031@tlaxiaco.tecnm.mx>
2025-03-19 22:17:10 -0600

    METODO 1 AGREGADO A BIBLIOTECA

--- Biblioteca_INE/Class1.cs
+++ Biblioteca_INE/Class1.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Biblioteca_INE
 {
-    public class Class1
+    public class Clase_validaciones
     {
         //BIBLIOTECA AÑADIDA
+        //MÉTODO DE CONFIRMACIÓN DE INICIO DE SESIÓN_FORM 1
+        public static Boolean Validar_form1(string C_correo, string C_contraseña)//metodo para saber si los campos de texto están vacíos o no
+        {
+            if (C_correo == "" && C_contraseña == "" || C_correo == "" ||
+                C_contraseña == "")//validación de los campos de texto, que no estén vacios
+            {
+                DialogResult result = MessageBox.Show("INGRESE DATOS", "confirmación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);//mensaje de alerta
+                return false;//retorno del valor boleano
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
